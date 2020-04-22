68f6615ffbabc486a3694b3ef75b6b83983e46e7
harry <459884392@qq.com>
2020-04-23 00:29:48 +0800

    add annotation

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var config = {
     keywords: [
         '食物语', "恋与制作人"
-    ],
+    ], // 搜索关键字
     keywordsBlacklist: [
-    ],
+    ], // 屏蔽的关键词
     tids: '', // '' for all
-    isUnion: false,
-    filePath: './bilibiliSearch.xlsx'
+    isUnion: false, //是否取并集，false代表取交集
+    filePath: './bilibiliSearch.xlsx' //存文件的路径
 }
 
 module.exports = config
