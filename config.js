61cee29e7c42d28b03fe80a6ded8e2d97539b898
harry <459884392@qq.com>
2020-04-23 00:09:36 +0800

    add config.js and fix bug

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {
+    keywords: [
+        '食物语', "恋与制作人"
+    ],
+    keywordsBlacklist: [
+    ],
+    tids: '', // '' for all
+    isUnion: false,
+    filePath: './bilibiliSearch.xlsx'
+}
+
+module.exports = config
